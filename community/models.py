d490f3b6f2704375536e3a046c4c30558f05b4a8
kdyddd <kdyddd@gmail.com>
2021-11-19 13:41:09 +0900

    11191341

--- community/models.py
+++ community/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from accounts.models import User
 # Create your models here.
 
+class Genre(models.Model):
+    name = models.TextField()
+
+class Movie_data(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    vote_average = models.FloatField()
+    release_date = models.DateField()
+    poster_path = models.TextField()
+    video = models.TextField()
+    genre_ids = models.ManyToManyField(Genre)
+
 class Community_review(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
