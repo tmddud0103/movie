c46df742cb1dc42b5b8764669348080989484424
kdyddd <kdyddd@gmail.com>
2021-11-23 16:12:43 +0900

    중간저장

--- movies/views.py
+++ movies/views.py
@@ -20,9 +20,24 @@ def index(request):
 def detail(request, pk):
 
     movie_data = get_object_or_404(Movie_data, pk=pk)
+    star = []
+    star_fill = []
+    star_half = []
+
+    for i in range(int(movie_data.vote_average) // 2):
+        star_fill.append(1)
     
+    if int(movie_data.vote_average) % 2 == 1:
+        star_half.append(1)
+
+    for i in range(5-len(star_fill)-len(star_half)):
+        star.append(1)
+
     context = {
         'movie_data': movie_data,
+        'star': star,
+        'star_fill': star_fill,
+        'star_half': star_half,
     }
 
     return render(request, 'movies/detail.html', context)
